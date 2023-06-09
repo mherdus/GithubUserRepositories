16559ed9009b600ad8a046f5906050277271fe53
Mateusz Herduś <herdusmateusz@gmail.com>
2023-06-09 10:39:23 +0200

    Initial commit

---
+++ src/main/java/com/mherdus/githubuserrepositories/GithubUserRepositoriesApplication.java
@@ -0,0 +1,13 @@
+package com.mherdus.githubuserrepositories;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubUserRepositoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubUserRepositoriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mherdus/githubuserrepositories/GithubUserRepositoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mherdus.githubuserrepositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubUserRepositoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
